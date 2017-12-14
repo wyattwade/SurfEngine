f56c63fde31ad74802b60b1a6f4b6a388614df46
Wyatt Elliott <wyattelliott27@gmail.com>
2017-12-13 19:27:11 -0800

    scrapper with hangfire schedule works in prod. Terminating it however

--- MySurfboardsMVP/OwinStartup.cs
+++ MySurfboardsMVP/OwinStartup.cs
@@ -1,36 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Hangfire;
-using Microsoft.Owin;
-using Owin;
-using MySurfboardsMVP.Controllers;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using Hangfire;
+//using Microsoft.Owin;
+//using Owin;
+//using MySurfboardsMVP.Controllers;
 
-[assembly: OwinStartup(typeof(MyWebApplication.OwinStartup))]
+//[assembly: OwinStartup(typeof(MyWebApplication.OwinStartup))]
 
-namespace MyWebApplication
-{
-    public class OwinStartup
-    {
-        public void Configuration(IAppBuilder app)
-        {
-            GlobalConfiguration.Configuration
-                .UseSqlServerStorage("SurfboardDBConnection");
+//namespace MyWebApplication
+//{
+//    public class OwinStartup
+//    {
+//        public void Configuration(IAppBuilder app)
+//        {
+//            GlobalConfiguration.Configuration
+//                .UseSqlServerStorage("SurfboardDBConnection");
 
-            app.UseHangfireDashboard();
-            app.UseHangfireServer();
-
-
-            // Background - tested and works
-            // BackgroundJob.Enqueue(() => Console.WriteLine("Fire-and-forget"));
+//            app.UseHangfireDashboard();
+//            app.UseHangfireServer();
 
 
+//            // Background - tested and works
+//            // BackgroundJob.Enqueue(() => Console.WriteLine("Fire-and-forget"));
 
-            // Recurring
-            RecurringJob.AddOrUpdate(() => ScrappingController.ScrappingFunction(), "*/10 * * * *");
 
 
-        }
-    }
-}
+//            // Recurring
+//            RecurringJob.AddOrUpdate(() => ScrappingController.ScrappingFunction(), "*/10 * * * *");
+
+
+//        }
+//    }
+//}
