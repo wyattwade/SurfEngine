872ca0af791f66a31d1bfbb73a6eaab2b15f0160
Wyatt Elliott <wyattelliott27@gmail.com>
2017-12-13 18:38:10 -0800

    hangfire dashboard visibility

--- MySurfboardsMVP/OwinStartup.cs
+++ MySurfboardsMVP/OwinStartup.cs
@@ -6,11 +6,34 @@ using Hangfire;
 using Microsoft.Owin;
 using Owin;
 using MySurfboardsMVP.Controllers;
+using Hangfire.Dashboard;
 
 [assembly: OwinStartup(typeof(MyWebApplication.OwinStartup))]
 
 namespace MyWebApplication
 {
+
+
+
+    public class MyAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            // In case you need an OWIN context, use the next line, `OwinContext` class
+            // is the part of the `Microsoft.Owin` package.
+            var owinContext = new OwinContext(context.GetOwinEnvironment());
+
+            // Allow all authenticated users to see the Dashboard (potentially dangerous).
+            // return owinContext.Authentication.User.Identity.IsAuthenticated;
+            return true;
+        }
+    }
+
+
+
+
+
+
     public class OwinStartup
     {
         public void Configuration(IAppBuilder app)
@@ -21,14 +44,24 @@ namespace MyWebApplication
             app.UseHangfireDashboard();
             app.UseHangfireServer();
 
+            app.UseHangfireDashboard("/hangfire", new DashboardOptions
+            {
+                Authorization = new[] { new MyAuthorizationFilter() }
+            });
+
+
 
             // Background - tested and works
             // BackgroundJob.Enqueue(() => Console.WriteLine("Fire-and-forget"));
 
 
-
-            // Recurring
+            // Recurring function (daily). Runs the scraping function.
             RecurringJob.AddOrUpdate(() => ScrappingController.ScrappingFunction(), Cron.Daily);
+
+
+
+
+
 
 
         }
